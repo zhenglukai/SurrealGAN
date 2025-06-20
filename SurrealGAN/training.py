94a671ac86786406ed92bf6aeba0e6479ef3c5b8
Zheng LK <36380155+zhenglukai@users.noreply.github.com>
2025-06-20 01:03:35 -0700

    Revert "Update training.py"
    This reverts commit c584906510a1ff18b45c28d3c367398c0107c0c4.

--- SurrealGAN/training.py
+++ SurrealGAN/training.py
@@ -176,7 +176,6 @@ class Surreal_GAN_train:
         if not verbose:
             pbar = tqdm(total=self.opt.final_saving_epoch + 2000)  # type: ignore
         for epoch in range(1, self.opt.final_saving_epoch + 2001):  # type: ignore
-            losses = {}  # Initialize to avoid UnboundLocalError
             if not verbose:
                 pbar.update(1)
             # epoch_start_time = time.time()
